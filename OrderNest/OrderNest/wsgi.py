441674a1ffaa92e504279bc4db9fbdcef0e2c1a2
Abdullah Alsofayan <alsofayan10@gmail.com>
2025-07-21 21:49:08 +0300

    create apps, setup settings, start with base.html

---
+++ OrderNest/OrderNest/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for OrderNest project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OrderNest.settings')
+
+application = get_wsgi_application()
